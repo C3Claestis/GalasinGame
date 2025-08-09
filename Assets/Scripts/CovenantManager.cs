006332342ef6754637da4fdd062bd79ead4330ad
MJ Sidik <darkimanity@gmail.com>
2025-08-09 13:33:58 +0700

    Update GUI untuk main game

--- Assets/Scripts/CovenantManager.cs
+++ Assets/Scripts/CovenantManager.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CovenantManager : MonoBehaviour
 {
+    [SerializeField] int enemyRound;
+    [SerializeField] Text covenantText;
+    [SerializeField] Text loseText;
+    
+    private DrawPathMovement[] players;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (players == null || players.Length == 0)
+        {
+            players = FindObjectsByType<DrawPathMovement>(FindObjectsSortMode.None);
+        }
+
+        covenantText.text = players.Length.ToString();
+        loseText.text = enemyRound.ToString();
     }
 
     // Update is called once per frame
